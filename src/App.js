9984678ad23a0387ece7e41383995050a31791d9
youngjin <teen14y@gmail.com>
2020-07-20 14:26:42 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Camera from 'react-html5-camera-photo'
+import Webcam from 'react-webcam'
+import styled from 'styled-components';
 import './App.css';
+import 'react-html5-camera-photo/build/css/index.css';
 
 
 function App() {
@@ -11,9 +14,14 @@ function App() {
   return (
     <div className="App">
       camera
-      <Camera onTakePhoto={handleTakePhoto} onTakePhotoAnimationDone={handleTakePhoto}/>
+      <StyledCamera onTakePhoto={handleTakePhoto} onTakePhotoAnimationDone={handleTakePhoto}/>
+      
     </div>
   );
 }
+
+const StyledCamera = styled(Camera)`
+background-color: black
+`
 
 export default App;
